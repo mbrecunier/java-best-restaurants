02913ec4744235848a3347270021740591860a0a
Michelle Brecunier and Tyler Suderman <michelle.brecunier@gmail.com>
2016-02-24 08:48:38 -0800

    correctly instantiate Restaurant class

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -8,7 +8,15 @@ public class RestaurantTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
-  public void all_emptyAtFirst() {
-      assertEquals(Restaurant.all().size(), 0);
+  public void restaurant_instantiatesCorrectly_true() {
+    Restaurant newResty = new Restaurant("Cheryl's");
+    assertTrue(newResty instanceof Restaurant);
   }
+
+  // @Test
+  // public void all_emptyAtFirst() {
+  //     assertEquals(Restaurant.all().size(), 0);
+  // }
+
+
 }
