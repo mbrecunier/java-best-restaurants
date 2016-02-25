68608d5c5bc4d09602ae778cfa71384863c707f8
Michelle Brecunier and Tyler Suderman <tcsuder@gmail.com>
2016-02-24 16:25:48 -0800

    add form for creating new cuisine

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,11 +25,26 @@ public class App {
       int cuisineId = Integer.parseInt(request.queryParams("cuisineId"));
       Restaurant newResty = new Restaurant(name, cuisineId);
       newResty.save();
+      request.session().attribute("newResty", newResty);
+      model.put("cuisines", Cuisine.all());
+      model.put("newResty", newResty);
+      model.put("index", "templates/index.vtl");
+      model.put("template", "templates/cuisinecheck.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    post("/new/cuisine", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String type = request.queryParams("cuisine");
+      Cuisine newCuisine = new Cuisine(type);
+      newCuisine.save();
+      Restaurant newResty = request.session().attribute("newResty");
+      newResty.update(newResty.getName(), newCuisine.getId(), "", "", "");
       model.put("cuisines", Cuisine.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
 
     /******************************************************
     Students: TODO: Create page to add a new restaurant
